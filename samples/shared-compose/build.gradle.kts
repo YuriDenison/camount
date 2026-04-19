7a3764d65829c0c8ec3ffa6f73d0d29bfa9500a4
Yuri Denison <yuri.denison@gmail.com>
2026-04-19 20:53:47 +0300

    Scaffold samples/shared-compose module

---
+++ samples/shared-compose/build.gradle.kts
@@ -0,0 +1,44 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+  alias(libs.plugins.kotlinMultiplatform)
+  alias(libs.plugins.androidKmpLibrary)
+  alias(libs.plugins.composeMultiplatform)
+  alias(libs.plugins.composeCompiler)
+}
+
+kotlin {
+  androidLibrary {
+    namespace = "io.denison.camount.sample.shared"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    minSdk = libs.versions.android.minSdk.get().toInt()
+
+    compilerOptions {
+      jvmTarget.set(JvmTarget.JVM_11)
+    }
+  }
+
+  listOf(iosArm64(), iosSimulatorArm64()).forEach { iosTarget ->
+    iosTarget.binaries.framework {
+      baseName = "SampleShared"
+      isStatic = true
+    }
+  }
+
+  sourceSets {
+    commonMain.dependencies {
+      implementation(project(":camount"))
+      implementation(compose.runtime)
+      implementation(compose.foundation)
+      implementation(compose.ui)
+      implementation(compose.material3)
+      implementation(compose.components.resources)
+    }
+  }
+}
+
+compose.resources {
+  publicResClass = true
+  packageOfResClass = "io.denison.camount.sample.resources"
+  generateResClass = always
+}
